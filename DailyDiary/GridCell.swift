a55d7459a316a930eed3d283ff1140b384cc9ebe
swillsea <sam.willsea@gmail.com>
2016-05-09 20:01:24 -0700

    InitialVCs, storyboard setup

---
+++ DailyDiary/GridCell.swift
@@ -0,0 +1,13 @@
+//
+//  GridCell.swift
+//  DailyDiary
+//
+//  Created by Sam on 5/9/16.
+//  Copyright © 2016 Sam Willsea. All rights reserved.
+//
+
+import UIKit
+
+class GridCell: UICollectionViewCell {
+    
+}
---
+++ DailyDiary/ListCell.swift
@@ -0,0 +1,13 @@
+//
+//  ListCell.swift
+//  DailyDiary
+//
+//  Created by Sam on 5/9/16.
+//  Copyright © 2016 Sam Willsea. All rights reserved.
+//
+
+import UIKit
+
+class ListCell: UICollectionViewCell {
+    
+}
