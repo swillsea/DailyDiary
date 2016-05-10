7de45b94b4fdd7fb227e203e25f4a19ee864b05a
swillsea <sam.willsea@gmail.com>
2016-05-09 22:18:50 -0700

    CameraActions ready and textView becomes first responder automatically on AddEditVC

--- DailyDiary/AddOrEditVC.swift
+++ DailyDiary/AddOrEditVC.swift
@@ -8,24 +8,32 @@
 
 import UIKit
 
-class AddOrEditVC: UIViewController {
+class AddOrEditVC: UIViewController, UIActionSheetDelegate, UITextViewDelegate {
 
     @IBOutlet weak var entryText: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        entryText.becomeFirstResponder()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 
     @IBAction func onCameraButtonPressed(sender: UIBarButtonItem) {
         
+        let prompt = UIAlertController(title:nil, message:nil, preferredStyle: .ActionSheet)
+        let firstAction = UIAlertAction(title: "Use Camera", style: .Default) { (alert: UIAlertAction!) -> Void in
+            //launch cameraVC
+        }
+        let secondAction = UIAlertAction(title: "Choose from Library", style: .Default) { (alert: UIAlertAction!) -> Void in
+            //launch importVC
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .Cancel) { (alert: UIAlertAction) in
+            //do nothing
+        }
+        prompt.addAction(firstAction)
+        prompt.addAction(secondAction)
+        prompt.addAction(cancel)
+        presentViewController(prompt, animated: true, completion:nil)
     }
     
 }
